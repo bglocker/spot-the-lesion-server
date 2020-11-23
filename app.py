8682fcacf4fca4caca02a8e755802b0d5470cd6c
Calin Biberea <calinbiberea@gmail.com>
2020-11-24 00:04:05 +0200

    Update increment

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 # app.py
+from firebase import Firebase
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+config = {
+    "apiKey": "",
+    "authDomain": "spot-the-lesion.firebaseapp.com",
+    "databaseURL": "https://spot-the-lesion.firebaseio.com/",
+    "projectId": "spot-the-lesion",
+    "storageBucket": "spot-the-lesion.appspot.com",
+    "messagingSenderId": "131387805123",
+    "appId": "1:131387805123:web:9bdbabe358ffcf04ad4176",
+    "measurementId": "G-13PZY5QQPK"
+}
 
 
 @app.route('/getmsg/', methods=['GET'])
