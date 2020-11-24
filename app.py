de428c562e6db241fcc77e57a9efb7afe69b4cd4
Calin Biberea <calinbiberea@gmail.com>
2020-11-24 12:58:16 +0200

    Cors more

--- app.py
+++ app.py
@@ -25,60 +25,17 @@ storage = firebase.storage()
 storage.child("sort_image/img.png").put("img.png")
 
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
 @app.route('/post/', methods=['POST'])
 @cross_origin()
 def post_something():
-    param = request.form.get('name')
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+    return True
 
 
 # A welcome message to test our server
 @app.route('/')
+@cross_origin()
 def index():
     return "<h1>Welcome to our server !!</h1>"
-
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    return response
 
 
 if __name__ == '__main__':
