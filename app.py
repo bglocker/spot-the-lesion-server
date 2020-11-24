ccfcc10ef4ffb69fdff516035fc6bd4d039f9134
Calin Biberea <calinbiberea@gmail.com>
2020-11-24 12:15:16 +0200

    More cors policy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os
 from firebase import Firebase
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
